38822582740a97cbb2c32ce20b1a24293efa6e72
Ryo Nonoyama <ryounonoyama@gmail.com>
2024-11-13 18:17:50 +0900

    fix: expect method syntax in foo.test.js

--- tests/foo.test.js
+++ tests/foo.test.js
@@ -2,7 +2,7 @@ const chai = require("chai");
 const expect = chai.expect;
 
 describe("first test", () => {
-  it("should run this test", async () => {
-    expect(false).to.be(true);
+  it("should run this test", () => {
+    expect(false).to.be.true;
   });
 });
