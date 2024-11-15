c439e9d6d023274bd95fd465d21c9545ce22b0e0
Ryo Nonoyama <ryounonoyama@gmail.com>
2024-11-15 16:43:34 +0900

    feat: add mission screen

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -8,9 +8,25 @@ function App() {
   const [randomSelectedBooks, setRandomSelectedBooks] = useState([]);
   //const [userId, setUserId] = useState(1);
   const [currentView, setCurrentView] = useState("Home");
+  const [formData, setFormData] = useState({
+    keyword: "",
+    reason: "",
+    comment: ""
+  });
 
   function setCurrentViewToHome() {
     setCurrentView("Home");
+  }
+
+  function handleSubmit() {
+    console.log("submit!");
+  }
+
+  function updateFormData(e) {
+    setFormData(prevData => ({
+      ...prevData,
+      [e.target.name]: e.target.value
+    }));
   }
 
   useEffect(() => {
@@ -32,23 +48,87 @@ function App() {
   }, []);
 
   console.log(currentView);
+  console.log(formData);
   return (
     <>
       <Navbar setCurrentViewToHome={setCurrentViewToHome} />
-      <div>
-        <h1>TODAY TSUNDOKU</h1>
-        <img className="image" src={randomSelectedBooks.cover_image_url} />
-      </div>
-      <h2>MY TSUNDOKU COLLECTION</h2>
-      <ul>
-        {books.map(book => {
-          return (
-            <li className="imageCell" key={book.id}>
-              <img className="image" src={book.cover_image_url} alt="None" />
-            </li>
-          );
-        })}
-      </ul>
+      {currentView === "Home" ? (
+        <>
+          <div>
+            <h1>TODAY TSUNDOKU</h1>
+            <img
+              className="image"
+              src={randomSelectedBooks.cover_image_url}
+              onClick={() => {
+                setCurrentView("Event");
+              }}
+            />
+          </div>
+          <h2>MY TSUNDOKU COLLECTION</h2>
+          <ul>
+            {books.map(book => {
+              return (
+                <li className="imageCell" key={book.id}>
+                  <img
+                    className="image"
+                    src={book.cover_image_url}
+                    alt="None"
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      ) : (
+        <>
+          <div>
+            <h1>MISSION!</h1>
+            <div>制限時間以内に本の中から1つのキーワードを選択し入力せよ！</div>
+            <img
+              className="image"
+              src={randomSelectedBooks.cover_image_url}
+              onClick={() => {
+                setCurrentView("Event");
+              }}
+            />
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>
+                keyword:
+                <input
+                  type="text"
+                  name="keyword"
+                  value={formData.keyword}
+                  onChange={updateFormData}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                reason:
+                <input
+                  type="text"
+                  name="reason"
+                  value={formData.reason}
+                  onChange={updateFormData}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                comment:
+                <textarea
+                  name="comment"
+                  value={formData.comment}
+                  onChange={updateFormData}
+                />
+              </label>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </>
+      )}
     </>
   );
 }
