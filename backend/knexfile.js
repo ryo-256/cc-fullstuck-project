c3b51b789ca0a8314b0968e7b1d90b39e61b607c
Ryo Nonoyama <ryounonoyama@gmail.com>
2024-11-17 10:09:07 +0900

    chore: create Dockerfile and docker-compose.yml

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -7,9 +7,11 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: {
+      host: process.env.DB_HOST || "localhost",
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      port: parseInt(process.env.DB_PORT),
     },
     migrations: {
       directory: "./db/migrations",
