e5ccbda45a6756922c9fc22c3b3f0714fc243172
Ryo Nonoyama <ryounonoyama@gmail.com>
2024-11-18 19:05:14 +0900

    fix: docker environment

--- backend/db/seeds/20241114033040_initial_books.js
+++ backend/db/seeds/20241114033040_initial_books.js
@@ -4,8 +4,6 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex("user_book_events").del();
-  await knex("user_books").del();
   await knex("books").del();
   await knex("books").insert([
     {
