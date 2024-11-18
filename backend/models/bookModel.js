48489d5dcc4f4c24101c8950c5265321a4ae00c1
Ryo Nonoyama <ryounonoyama@gmail.com>
2024-11-18 13:32:47 +0900

    add: /api/user-books/:userBookId/events

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const knex = require("../db/knex");
 module.exports = {
   async findAllBooks() {
@@ -8,5 +9,21 @@ module.exports = {
       .join("user_books", "books.id", "=", "user_books.book_id")
       .where("user_books.user_id", userId)
       .select("books.*");
+  },
+  async createEvent(userBookId, eventData) {
+    try {
+      await knex.transaction(async (trx) => {
+        const [eventId] = await trx("events")
+          .insert(eventData)
+          .returning("id");
+        const userBookEvent = {
+          event_id: parseInt(eventId.id),
+          user_book_id: parseInt(userBookId),
+        };
+        await trx("user_book_events").insert(userBookEvent);
+      });
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
